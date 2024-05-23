9b34e021e318d5b46a82ad0a112d662ccace60aa
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2024-05-23 19:45:44 +0900

    引数を渡してもQuineになるようにした

--- rubykaigi_v1.rb
+++ rubykaigi_v1.rb
@@ -1,6 +1,6 @@
 # 2024 => 2025
-eval$s=%w'bins={"next"=>["BAhsK1P+/wAAAP9/+P/f//+A/+D/f/7/9/9/+P/4/7////3Bnz/+/uDv/38A8OcHPwD4+wMAAPz5gA8A/v4AAOA/PuAD8J//f8D/hw/44P/j/z/8f+ADPv4/+P+f/wP4gM//AQDg/wcAPuD7AwAA+P8BgB/8/gDgD/5/wO+Pvz/g///////j/+P///3/9///4D/4/3/8f/z/PwAA/v8f/Ac=",78]};
-bin,x=bins[ARGV[0]]||["BAhsK0/+/wAAAP9/gP/8/w/4D/7/B//z/3/4//j/P/7PH/z54+8P/vw/APDnBz8A+Pv9AMCfD/gA4P/zA+A/PuAD8P/HD/x/+IAP/v////x/4AM+/j////s/gA/4/B/8//8HAD7g+wMAwM8fAPjB7w8AAD9/wO+Pvz/gB/z8/z/+P/7/H/z////gP/j/f/D///8DAOD//8H/",74];
+eval$s=(["_=\"#{ARGV[0]}\";"]+%w'bins={"next"=>["BAhsK1P+/wAAAP9/+P/f//+A/+D/f/7/9/9/+P/4/7////3Bnz/+/uDv/38A8OcHPwD4+wMAAPz5gA8A/v4AAOA/PuAD8J//f8D/hw/44P/j/z/8f+ADPv4/+P+f/wP4gM//AQDg/wcAPuD7AwAA+P8BgB/8/gDgD/5/wO+Pvz/g///////j/+P///3/9///4D/4/3/8f/z/PwAA/v8f/Ac=",78]};
+bin,x=bins[_]||["BAhsK0/+/wAAAP9/gP/8/w/4D/7/B//z/3/4//j/P/7PH/z54+8P/vw/APDnBz8A+Pv9AMCfD/gA4P/zA+A/PuAD8P/HD/x/+IAP/v////x/4AM+/j////s/gA/4/B/8//8HAD7g+wMAwM8fAPjB7w8AAD9/wO+Pvz/gB/z8/z/+P/7/H/z////gP/j/f/D///8DAOD//8H/",74];
 n=Marshal.load(bin.unpack("m")[0]);
 y=16;
 e="eval$s=%w"<<39<<($s*3);
@@ -10,4 +10,4 @@ j=-1;
 o[-8,6]=""<<39<<".join";
 puts(o);
 #
-'.join
+').join
--- rubykaigi_v2.rb
+++ rubykaigi_v2.rb
@@ -1,6 +1,6 @@
 # 2024 => 20024
-eval$s=%w'bins={"matz"=>["BAhsK2L+/wAAAAAA+P8D/Of/f8B/AP6D///B//z/H/4/8P/x/3/8nz/488efP/7+4M//AwB/fvDzgx8A/P1+AODPB3w+4AOA/88PgP/4gM8HfAD+//iB/w8f8PmAD/7////8f+ADPh/w8f/5/9//AXzA5wM+/wf///8BgA/4fMD3BwCAnz8A8IOfH/z+AADw8wf8/vjzx98f8AN+/v8f/x/4//j/f/D///+D/wD8B///D/7//38AAAAA4P//wf8=",93]};
-bin,x=bins[ARGV[0]]||["BAhsK0/+/wAAAP9/gP/8/w/4D/7/B//z/3/4//j/P/7PH/z54+8P/vw/APDnBz8A+Pv9AMCfD/gA4P/zA+A/PuAD8P/HD/x/+IAP/v////x/4AM+/j////s/gA/4/B/8//8HAD7g+wMAwM8fAPjB7w8AAD9/wO+Pvz/gB/z8/z/+P/7/H/z////gP/j/f/D///8DAOD//8H/",74];
+eval$s=(["_=\"#{ARGV[0]}\";"]+%w'bins={"matz"=>["BAhsK2L+/wAAAAAA+P8D/Of/f8B/AP6D///B//z/H/4/8P/x/3/8nz/488efP/7+4M//AwB/fvDzgx8A/P1+AODPB3w+4AOA/88PgP/4gM8HfAD+//iB/w8f8PmAD/7////8f+ADPh/w8f/5/9//AXzA5wM+/wf///8BgA/4fMD3BwCAnz8A8IOfH/z+AADw8wf8/vjzx98f8AN+/v8f/x/4//j/f/D///+D/wD8B///D/7//38AAAAA4P//wf8=",93]};
+bin,x=bins[_]||["BAhsK0/+/wAAAP9/gP/8/w/4D/7/B//z/3/4//j/P/7PH/z54+8P/vw/APDnBz8A+Pv9AMCfD/gA4P/zA+A/PuAD8P/HD/x/+IAP/v////x/4AM+/j////s/gA/4/B/8//8HAD7g+wMAwM8fAPjB7w8AAD9/wO+Pvz/gB/z8/z/+P/7/H/z////gP/j/f/D///8DAOD//8H/",74];
 n=Marshal.load(bin.unpack("m")[0]);
 y=16;
 e="eval$s=%w"<<39<<($s*3);
@@ -10,4 +10,4 @@ j=-1;
 o[-8,6]=""<<39<<".join";
 puts(o);
 #
-'.join
+').join
